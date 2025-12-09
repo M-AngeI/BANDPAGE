18c9e9ef90c0dcf0f1b9e661057b26f8af554c00
M-AngeI <mabenitez59@soy.sena.edu.co>
2025-12-09 18:01:32 -0500

    Arreglo imagenes y componentes

--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -18,30 +18,30 @@ export class products {
    {
     id: 1,
     nombre: 'scaled and icy',
-    descripcion: '4to almbum de twenty one pilots',
+    descripcion: '4to album de twenty one pilots',
     precio: 80000,
-    imagen: '/imagenes/ScaledAndIcy.jpg'
+    imagen: './imagenes/ScaledAndIcy.jpg'
    },
    {
     id: 2,
     nombre: 'blurryface',
     descripcion:'el album mas famoso de la banda',
     precio: 120000,
-    imagen: '/imagenes/blurryface.jpg'
+    imagen: './imagenes/blurryface.jpg'
    },
    {
     id: 3,
     nombre: 'breach',
     descripcion: 'el disco mas nuevo de la banda',
     precio: 100000,
-    imagen: '/imagenes/breach.jpg'
+    imagen: './imagenes/breach.jpg'
    },
    {
     id: 4,
     nombre: 'clancy',
     descripcion: 'de los albumes mejor puntuados',
     precio: 90000,
-    imagen: '/imagenes/clancy.jpg'
+    imagen: './imagenes/clancy.jpg'
    }
   ]
   constructor() { }
